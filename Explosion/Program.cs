2c718f93ecfb19798acd98a6968d7b2026ae40e3
sunnywiz <sunnywiz@gmail.com>
2015-01-03 22:39:45 -0500

    Now exploding through different rocks
    with different forces

--- Explosion/Program.cs
+++ Explosion/Program.cs
@@ -14,14 +14,14 @@ namespace Explosion
             try
             {
                 var pattern1 = new string[] {
-                "...... ....",
-                "..@@..  ...",
-                "..@@.. ....",
-                "...... ....",
-                ".....@ ....",
-                "....@@ @...",
-                "...... ....",
-                "..@@.......",
+                "@@@@@@     ",
+                "@@@@@@     ",
+                "@@@@@@     ",
+                "@@@@@@     ",
+                "@@@@@@     ",
+                "...........",
+                "...........",
+                "...........",
                 "...........",
                 "...........",
             };
@@ -29,21 +29,31 @@ namespace Explosion
                 space.LoadFromString(pattern1,null,null,x=>x);
 
                 var forceMap = new TwoDSpace<double>();
-                forceMap.Set(0, 0, 6); 
+                forceMap.Set(0, 0, 15); 
 
 
-                CircleMapper.VisitFromCenter(6, (p) =>
+                CircleMapper.VisitFromCenter(10, (p) =>
                 {
                     // calculate available force
                     if (p.Parents.Count == 0) return;
                     var sum = p.Parents.Sum(x => x.Ratio);
                     var force = p.Parents.Sum(x => (forceMap.Get(x.X, x.Y) * x.Ratio) / sum);
-                    force = force - 1;
-                    if (force < 0) force = 0; 
+
+                    var ratio = 0.9;
+                    var ch = space.Get(p.X, p.Y);
+                    switch (ch)
+                    {
+                        case '.': ratio = 0.9; break;
+                        case '@': ratio = 0.5; break;
+                        case ' ': ratio = 0.2; break; 
+                    }
+                    
+                    force = force * ratio;
+                    if (force < 1) force = 0; 
                     forceMap.Set(p.X, p.Y, force); 
                 }); 
 
-                forceMap.ConsoleWriteLine(Console.Out, (c) => c.ToString("c"));
+                forceMap.ConsoleWriteLine(Console.Out, (f) => String.Format(" {0:00}",f));
 
             }
             catch (Exception ex)
